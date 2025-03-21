431af6cbc6d907891242c5bc8390daacdf5a64a3
Minamaher-sw <mosadefmena185@gmail.com>
2025-03-22 01:34:41 +0200

    modify code remove setinterval and create two method changeOpicity and returnOpicity method

--- JS/Day4/lab/JS/ex3.js
+++ JS/Day4/lab/JS/ex3.js
@@ -5,86 +5,70 @@ var img4 =document.images[3]
 
 var opacity_value_1 = 1 ;
 var opacity_value_2 = 1 ;
-var opacity_value_3 = 1 ;
-var opacity_value_4 = 1 ;
 let inter1 ;
 let inter2 ;
-let inter3 ;
-let inter4 ;
 
-
-img1.onmouseover =function()
-{  
+function changeOpacity(imag)
+{
     clearInterval(inter1)
      opacity_value_1 = 1;
     inter1 = setInterval(
         function()
         {
-            img1.style.opacity=opacity_value_1 ;
+            imag.style.opacity=opacity_value_1 ;
             opacity_value_1 -= .1 ;
         } , 300)
 }
-img2.onmouseover =function()
+function returnOpacity(imgs)
 {
-
+    clearInterval(inter2)
+    opacity_value_2 = .3 ;
     inter2 = setInterval(
         function()
         {
-            img2.style.opacity=opacity_value_2 ;
-            opacity_value_2 -= .1 ;
-        } , 300)
-}
-img3.onmouseover =function()
-{
-    inter3 = setInterval(
-        function()
-        {
-            img3.style.opacity=opacity_value_3;
-            opacity_value_3 -= .1 ;
-            if(opacity_value_3 == 0)
-            {
-                opacity_value_3 = 1 ;
-            }
-        } , 300)
-}
-img4.onmouseover =function()
-{
-    inter4 = setInterval(
-        function()
-        {
-            img4.style.opacity=opacity_value_4 ;
-            opacity_value_4 -= .1 ;
-            if(opacity_value_4 == 0)
-            {
-                opacity_value_4 = 1 ;
-            }
-        } , 300)
-}
-img1.onmouseout =function()
-{
-    clearInterval(inter1)
-    opacity_value_1 = .3 ;
-    inter1 = setInterval(
-        function()
-        {
-            img1.style.opacity=opacity_value_1 ;
-            opacity_value_1 += .1 ;
-            if(opacity_value_1 == 1)
+            imgs.style.opacity=opacity_value_2 ;
+            opacity_value_2 += .1 ;
+            if(opacity_value_2 == 1)
             {
                 opacity_value_1 = 1;
             }
         } , 200)
 }
+img1.onmouseover =function()
+{  
+    changeOpacity(img1) ;
+}
+img2.onmouseover =function()
+{
+
+    changeOpacity(img2) ;
+}
+img3.onmouseover =function()
+{
+    changeOpacity(img3) ;
+}
+img4.onmouseover =function()
+{
+    changeOpacity(img4) ;
+    
+}
+img1.onmouseout =function()
+{
+    returnOpacity(img1) ;
+}
 img2.onmouseout =function()
 {
 
-    rerurnOpacity(img2)
+    returnOpacity(img2) ;
+
 }
 img3.onmouseout =function()
 {
-    rerurnOpacity(img3)
+    returnOpacity(img3) ;
+
 }
 img4.onmouseout =function()
 {
-    rerurnOpacity(img4)
+    returnOpacity(img4) ;
+
 }
