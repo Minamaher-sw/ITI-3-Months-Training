ec6874fb1ceba8fa6d77d28715705444b0475ada
Minamaher-sw <mosadefmena185@gmail.com>
2025-05-24 01:03:32 +0300

    adding model of user collection , setting schema of user , midlleWare for save and toJson to transform ret object

---
+++ Node js/Day5/lab/backend/models/user.js
@@ -0,0 +1,61 @@
+import  * as mongoosed from 'mongoose' 
+import bcrypt from "bcryptjs";
+const {Schema} = mongoosed.mongoose
+
+const userSchema = new Schema({
+    userName: {
+        type:String,
+        minlength:3,
+        maxlength:15,
+        required:true,
+        unique: true
+    },
+
+    firstName: {
+        type: String,
+        minlength:3,
+        maxlength:15,
+        required:true,
+    },
+
+    lastName: {
+        type: String,
+        minlength:3,
+        maxlength:15,
+        required:true,
+    },
+    dob: {
+        type: Date
+    },
+    Password: {
+        type: String,
+        minlength:3,
+        maxlength:15,
+        required:true
+    },
+    username: {
+        type: String,
+    }
+},{
+    // apply on any object ret from user model
+    toJSON: {
+        transform: function (doc, ret,options) {
+            delete ret.Password ;
+            delete ret.username ;
+            delete ret.__v ;
+            ret.fullName = ret.firstName +" "+ret.lastName;
+        }
+    }
+})
+
+// hashing by using salting middle ware before save info 
+userSchema.pre("save",function(){
+    const hash =bcrypt.hashSync(this.Password ,8);
+    this.Password =hash
+})
+
+const User = mongoosed.mongoose.model("User",userSchema)
+
+export {
+    User
+} 
