a799787be7638009e99c420931dfb842f34c6810
Minamaher-sw <mosadefmena185@gmail.com>
2025-05-24 17:59:28 +0300

    adding new root gettodobyid and adding authorize of user is admin or user

--- Node js/Day5/lab/backend/roots/todo.js
+++ Node js/Day5/lab/backend/roots/todo.js
@@ -5,13 +5,19 @@ import {
     getAllTodo,
     deleteTodo,
     updateTodo,
+    getTodoByid
 } from "../controllers/todo.js"
 
+import {authorize} from "../middlewares/authorize.js";
 // todo 
-todoRouter.post( "/",
+todoRouter.post( 
+    "/",
+    authorize(["admin","user"]),
     async (req, res,next) => {
     try{
-        const createdMeesage =await createTodo(req.body);
+        const title = req.body.title;
+        const user = req.user;
+        const createdMeesage = await createTodo({ title, userID: user._id });
         res.status(201).send(createdMeesage);
     }catch(error){
         next(error);
@@ -21,16 +27,35 @@ todoRouter.post( "/",
 
 todoRouter.get(
     "/",
+    authorize(["admin","user"]),
     async (req, res,next) => {
         try{
-            const todoList =await getAllTodo();
+            // get all todo that special to this user 
+            const userId =req.user._id;
+            console.log(userId)
+            const todoList =await getAllTodo(userId);
+            console.log(todoList);
             res.json(todoList);
         }catch(error){
             next(error)
         }
     }
 );
-todoRouter.patch("/:id",
+todoRouter.get(
+    "/:id",
+    authorize(["admin"]),
+    async (req, res,next) => {
+        try{
+            const todoList =await getTodoByid(req.params.id);
+            res.json(todoList);
+        }catch(error){
+            next(error)
+        }
+    }
+);
+todoRouter.patch(
+    "/:id",
+    authorize(["admin"]),
     async(req,res,next)=>{
         try{
             const todo = await updateTodo(req.params.id , req.body);
@@ -40,7 +65,9 @@ todoRouter.patch("/:id",
         }
 })
 
-todoRouter.delete("/:id",
+todoRouter.delete(
+    "/:id",
+    authorize(["admin","user"]),
     async (req,res,next)=>{
         try{
             const toodId = req.params.id;
